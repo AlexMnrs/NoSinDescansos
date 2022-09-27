54f4e9f3588c35e770639e4edee9b23b568ac3ff
AlexMnrs <alexmnrs92@gmail.com>
2022-09-27 10:55:24 +0200

    Small code improvements

--- NoSinDescansos/MainWindow.xaml.cs
+++ NoSinDescansos/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Diagnostics;
 using System.Media;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Threading;
 
@@ -18,17 +19,23 @@ namespace NoSinDescansos
         SoundPlayer player = new SoundPlayer();
         Registro registro = new Registro();
         TimeSpan ts;
+
         private string _tiempoActual = string.Empty;
         private int _tiempoJornadaEnMinutos;
-        private int _tiempoParaDescansarEnMinutos = 30;
         private int _tiempoInicioJornada;
-        private int _maximoJornadaEnMinutos = 480;
-        private int _minimoJornadaEnMinutos = 15;
+        private const int _TIEMPODEDESCANSOENMINUTOS = 30;
+        private const int _MAXIMOJORNADAENMINUTOS = 480;
+        private const int _MINIMOJORNADAENMINUTOS = 15;
         private int _descansos;
 
 
-        public int Descansos { get => _descansos; set => _descansos = value; }
+        public string TiempoActual { get => _tiempoActual; set => _tiempoActual = value; }
         public int TiempoJornadaEnMinutos { get => _tiempoJornadaEnMinutos; set => _tiempoJornadaEnMinutos = value; }
+        public int TiempoInicioJornada { get => _tiempoInicioJornada; set => _tiempoInicioJornada = value; }
+        public int TiempoDeDescansoEnMinutos => _TIEMPODEDESCANSOENMINUTOS;
+        public int MaximoJornadaEnMinutos => _MAXIMOJORNADAENMINUTOS;
+        public int MinimoJornadaEnMinutos => _MINIMOJORNADAENMINUTOS; 
+        public int Descansos { get => _descansos; set => _descansos = value; }
 
         public MainWindow()
         {
@@ -36,8 +43,8 @@ namespace NoSinDescansos
 
             timer.Tick += Timer_Tick;
             timer.Interval = new TimeSpan();
-            txtMinutos.Text = string.Format($"{_minimoJornadaEnMinutos} minutos");
-            TiempoJornadaEnMinutos = _minimoJornadaEnMinutos;
+            TiempoJornadaEnMinutos = MinimoJornadaEnMinutos;
+            txtMinutos.Text = string.Format($"{TiempoJornadaEnMinutos} minutos");
 
         }
 
@@ -51,25 +58,20 @@ namespace NoSinDescansos
             // Si el contador está en marcha...
             if (stopwatch.IsRunning)
             {
-                // Activamos el botón parar
-                btnParar.IsEnabled = true;
-                //Desactivamos botón iniciar
-                btnComenzar.IsEnabled = false;
-
                 // Guardamos el tiempo transcurrido
                 ts = stopwatch.Elapsed;
 
                 // Formateamos el tiempo en horas:minutos:segundos
-                _tiempoActual = string.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+                TiempoActual = string.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
 
                 // Asignamos 'tiempoActual' al texto del contador
-                txtContador.Text = _tiempoActual;
+                txtContador.Text = TiempoActual;
 
                 // Si hay descansos por hacer significa que todavía no hemos acabado nuestra jornada, así que continuamos.
                 while (ts.TotalMinutes < TiempoJornadaEnMinutos)
                 {
                     // Si han pasado 30 minutos desde que comenzamos la cuenta...
-                    if (ts.TotalMinutes - _tiempoInicioJornada >= _tiempoParaDescansarEnMinutos && Descansos > 0)
+                    if (ts.TotalMinutes - TiempoInicioJornada >= TiempoDeDescansoEnMinutos && Descansos > 0)
                     {
                         // Hacemos un descanso
                         Descansos--;
@@ -79,6 +81,9 @@ namespace NoSinDescansos
 
                         // Desactivamos botón parar
                         btnParar.IsEnabled = false;
+
+                        // Activamos botón resetear
+                        btnResetear.IsEnabled = true;
 
                         // Asignamos un sonido de alarma al reproductor
                         player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "\\alarma.wav";
@@ -92,8 +97,6 @@ namespace NoSinDescansos
                     return;
                 }
                 registro.listaRegistro.Add(new RegistroJornada(TiempoJornadaEnMinutos.ToString(), Descansos));
-                btnParar.IsEnabled = false;
-                btnComenzar.IsEnabled = false;
                 Trace.WriteLine(registro.listaRegistro.ToString());
                 stopwatch.Stop();
             }
@@ -107,12 +110,18 @@ namespace NoSinDescansos
         /// <param name="e"></param>
         private void btnComenzar_Click(object sender, RoutedEventArgs e)
         {
-            _tiempoInicioJornada = (int)ts.TotalMinutes;
+            btnComenzar.IsEnabled = false;
+            btnParar.IsEnabled = true;
+            btnIncrementar.IsEnabled = false;
+            btnDecrementar.IsEnabled = false;
+            btnResetear.IsEnabled = false;
+
+            TiempoInicioJornada = (int)ts.TotalMinutes;
 
             player.Stop(); // Paramos la alarma al continuar la cuenta atrás después de un descanso
 
             // Calculamos los descansos sólo la primera vez que ejecutamos el contador
-            if (Descansos == 0 && _tiempoInicioJornada == 0)
+            if (Descansos == 0 && TiempoInicioJornada == 0)
             {
                 Descansos = CalcularDescansos(TiempoJornadaEnMinutos);
             }
@@ -135,9 +144,10 @@ namespace NoSinDescansos
         {
             stopwatch.Reset();
             txtContador.Text = "00:00:00";
-            txtMinutos.Text = string.Format($"{_minimoJornadaEnMinutos} minutos");
+            txtMinutos.Text = string.Format($"{MinimoJornadaEnMinutos} minutos");
             btnComenzar.IsEnabled = true;
             btnParar.IsEnabled = false;
+            btnResetear.IsEnabled = false;
         }
 
         /// <summary>
@@ -149,6 +159,7 @@ namespace NoSinDescansos
         {
             btnComenzar.IsEnabled = true;
             btnParar.IsEnabled = false;
+            btnResetear.IsEnabled = true;
             stopwatch.Stop();
         }
 
@@ -170,9 +181,9 @@ namespace NoSinDescansos
         private void Incrementar_Click(object sender, RoutedEventArgs e)
         {
 
-            if (TiempoJornadaEnMinutos < _maximoJornadaEnMinutos)
+            if (TiempoJornadaEnMinutos < MaximoJornadaEnMinutos)
             {
-                TiempoJornadaEnMinutos = TiempoJornadaEnMinutos + 15;
+                TiempoJornadaEnMinutos += 15;
                 txtMinutos.Text = string.Format($"{TiempoJornadaEnMinutos} minutos");
             }
         }
@@ -185,9 +196,9 @@ namespace NoSinDescansos
         private void Decrementar_Click(object sender, RoutedEventArgs e)
         {
 
-            if (TiempoJornadaEnMinutos > _minimoJornadaEnMinutos)
+            if (TiempoJornadaEnMinutos > MinimoJornadaEnMinutos)
             {
-                TiempoJornadaEnMinutos = TiempoJornadaEnMinutos - 15;
+                TiempoJornadaEnMinutos -= 15;
                 txtMinutos.Text = string.Format($"{TiempoJornadaEnMinutos} minutos");
             }
         }
